8fa87679a0221df018b0331e6c320bcbe4041cc7
Sam Ojling <s.ojling@gmail.com>
2025-05-30 15:34:18 +0200

    feat: allowing anonymous sign-in

--- apps/obserdo-backend/src/lib/auth.ts
+++ apps/obserdo-backend/src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db/index.js"; // your drizzle instance
+import { anonymous } from "better-auth/plugins";
 
 // console.log("Here", db);
 
@@ -8,4 +9,5 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
+  plugins: [anonymous()],
 });
