7f96639a416fe283a3256af1d2160a7d012d49ee
Sam Ojling <s.ojling@gmail.com>
2025-05-30 15:23:03 +0200

    feat: installing better-auth and generating migrations

--- apps/obserdo-backend/src/db/index.ts
+++ apps/obserdo-backend/src/db/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "db/schema";
+import { schema } from "db/schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
