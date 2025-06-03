a4274ebe86d9796a40ed3a8a91bbeeaeff24b3a5
Sam Ojling <s.ojling@gmail.com>
2025-06-04 00:34:07 +0200

    fix: re-ran and removed all migrations when altering a bunch of columns

--- apps/obserdo-backend/drizzle/0005_broad_the_captain.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "tasks" ALTER COLUMN "completed" SET DEFAULT false;--> statement-breakpoint
-ALTER TABLE "todos" ALTER COLUMN "completed" SET DEFAULT false;
--- apps/obserdo-backend/drizzle/0006_careless_black_tom.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE "public"."todo_status" AS ENUM('todo', 'in-progress', 'done', 'archived');--> statement-breakpoint
-ALTER TABLE "todos" ADD COLUMN "status" "todo_status" DEFAULT 'todo' NOT NULL;--> statement-breakpoint
-ALTER TABLE "todos" DROP COLUMN "completed";
