14913c015d27aa347c6bba38569fef9e62b3b03f
Sam Ojling <s.ojling@gmail.com>
2025-06-09 00:31:50 +0200

    feat: copy to clipboard for shareable link

--- client/src/components/clipboard-button.tsx
+++ client/src/components/clipboard-button.tsx
@@ -1,31 +1,38 @@
-import { useEffect, useState } from "react"
-import { Button } from "./ui/button"
-import { CheckIcon, ClipboardIcon, Loader2Icon } from "lucide-react"
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { CheckIcon, ClipboardIcon, Loader2Icon } from "lucide-react";
 
+export function CopyButton({
+	value,
+	isLoading,
+}: { value: string; isLoading: boolean }) {
+	const [hasCopied, setHasCopied] = useState(false);
 
-export function CopyButton({ value, isLoading }: { value: string, isLoading: boolean }) {
-  const [hasCopied, setHasCopied] = useState(false)
+	useEffect(() => {
+		setTimeout(() => {
+			setHasCopied(false);
+		}, 2000);
+	}, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setHasCopied(false)
-    }, 2000)
-  }, [hasCopied])
-
-  return (
-    <Button
-      disabled={isLoading}
-      type='button'
-      className="w-full"
-      onClick={() => {
-        navigator.clipboard.writeText(value)
-        setHasCopied(true)
-      }}
-
-    >
-      <span className="sr-only">Copy</span>
-      {isLoading ? <Loader2Icon className="animate-spin" /> : hasCopied ? <CheckIcon /> : <ClipboardIcon />}
-      Copy link
-    </Button>
-  )
+	return (
+		<Button
+			disabled={isLoading}
+			type="button"
+			className="w-full"
+			onClick={() => {
+				navigator.clipboard.writeText(value);
+				setHasCopied(true);
+			}}
+		>
+			<span className="sr-only">Copy</span>
+			{isLoading ? (
+				<Loader2Icon className="animate-spin" />
+			) : hasCopied ? (
+				<CheckIcon />
+			) : (
+				<ClipboardIcon />
+			)}
+			Copy link
+		</Button>
+	);
 }
