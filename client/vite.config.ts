2bfc5575caf057c470314d4d8e3709011093564c
Sam Ojling <s.ojling@gmail.com>
2025-06-10 17:49:26 +0200

    fix: removing tsc during build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
